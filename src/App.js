8afdd212a36ab42c0d6ecab8bdce964076dd4690
Jears017 <turban01@mail.ru>
2021-11-23 16:56:10 +0300

    was added additional functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,81 @@
-import React from 'react'
-import './App.css';
+import React, {useState} from 'react'
 import {Main} from "./components/Main";
+import './App.css';
+
 
 export const App = () => {
-  return (
-    <div>
-      <Main/>
-    </div>
-  );
+    const [count, setCount] = useState([])
+    const add = () => {
+        let newArr = [...count]
+        setCount(newArr.reduce((total, amount) => {
+            if (amount % 2 === 0) {
+                total.push(amount + 1)
+            } else {
+                total.push(amount)
+            }
+            return total
+        }, []))
+        setCount(prev => {
+            return [...prev, 0]
+        })
+    }
+
+    const plusNumber = (idx) => {
+        let newArr = [...count]
+        newArr[idx]++
+        setCount(newArr)
+    }
+
+    const minusNumber = (idx) => {
+        let newArr = [...count]
+        newArr[idx]--
+        setCount(newArr)
+    }
+
+    const resetNumber = (idx) => {
+        let newArr = [...count]
+        newArr[idx] = 0;
+        setCount(newArr)
+    }
+
+    const deleteCounter = (idx) => {
+        let newArr = [...count]
+        setCount(newArr.filter((el, ind) => ind !== idx).reduce((total, amount) => {
+            if (amount % 2 !== 0) {
+                total.push(amount - 1)
+            } else {
+                total.push(amount)
+            }
+            return total
+        }, []))
+    }
+
+    const reset = () => {
+        setCount([])
+    }
+
+    return (
+        <div className='wrapper'>
+            <div className='count-buttons'>
+                <button onClick={add}>Add count</button>
+                <button onClick={reset}>Reset</button>
+            </div>
+            <div>Count: {count.length}</div>
+            <div>Sum: {count.reduce((sum, current) => sum + current, 0)}</div>
+            <div className='counters'>
+                {count.map((el, idx) => {
+                        return (
+                            <Main value={el} deleteCounter={() => deleteCounter(idx)}
+                                  reset={() => resetNumber(idx)} minusOne={() => minusNumber(idx)}
+                                  plusOne={() => plusNumber(idx)}
+                            />
+                        )
+                    }
+                )}
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,24 @@
-import React, {useState} from 'react'
+import React from 'react'
 import c from './Main.module.css'
 
-export const Main = () => {
-    const [number, setNumber] = useState(0)
+export const Main = ({value, plusOne, minusOne, reset, deleteCounter}) => {
     return (
-        <div className={number % 2 === 0 ? c.mainField + ' ' + c.even : c.mainField + ' ' + c.odd}>
+        <div className={value % 2 === 0 ? c.mainField + ' ' + c.even : c.mainField + ' ' + c.odd}>
             <div className={c.numberContainer}>
-                <span className={c.number}>{number}</span>
+                <span className={c.number}>{value}</span>
             </div>
             <div className={c.infoNumberContainer}>
-                {number % 2 === 0 ? <div className={c.infoNumber}>Введено чётное число</div> :
+                {value % 2 === 0 ? <div className={c.infoNumber}>Введено чётное число</div> :
                     <div className={c.infoNumber}>Введено нечетное число</div>}
             </div>
             <div className={c.buttonsContainer}>
                 <div className={c.buttonsGroup}>
-                    <button disabled={number <= 0} onClick={() => setNumber(number - 1)}>-</button>
-                    <button onClick={() => setNumber(0)}>Reset</button>
-                    <button onClick={() => setNumber(number + 1)}>+</button>
+                    <button disabled={value <= 0} onClick={() => minusOne()}>-</button>
+                    <button onClick={() => reset()}>Reset</button>
+                    <button onClick={() => plusOne()}>+</button>
                 </div>
             </div>
+                <button onClick={() => deleteCounter()}>delete</button>
         </div>
     )
 }
